1e8e28ac69d4920a4bc87e5eb38bca752e046d88
misammehmannavaz <misammehmannavaz@gmail.com>
2023-09-29 23:02:12 +0330

    added a new app and some other changes.

---
+++ account/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class UserRegistrationForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder':'your username'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'your email'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'your password'}))
