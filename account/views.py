05ed9620acefb7f61d6eadbea774e328a1bd5213
misammehmannavaz <misammehmannavaz@gmail.com>
2023-09-30 15:05:35 +0330

    make code reuseable (form_class)

--- account/views.py
+++ account/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import UserRegistrationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 class RegisterView(View):
+    form_class = UserRegistrationForm
     def get(self, request):
-        form = UserRegistrationForm()
+        form = self.form_class()
         return render(request, 'account/register.html', {'form': form})
     def post(self, request):
-        pass
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['username'], cd['email'], cd['password'])
+            messages.success(request, 'you registered successfully',
+                             'success')
+            return redirect('home:home')
         
 
