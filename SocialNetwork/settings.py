c455c5f3510d6f9e5d1b44479e620b9ad48affba
misammehmannavaz <misammehmannavaz@gmail.com>
2023-10-01 16:24:40 +0330

    Change the database

--- SocialNetwork/settings.py
+++ SocialNetwork/settings.py
@@ -76,9 +76,18 @@ WSGI_APPLICATION = 'SocialNetwork.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sn',
+        'USER': 'misam',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        
     }
 }
 
