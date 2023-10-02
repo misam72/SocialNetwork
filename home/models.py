cc2266e535a84d92e135f84765cc31af64b798ec
misammehmannavaz <misammehmannavaz@gmail.com>
2023-10-02 17:31:11 +0330

    auto_now date and time in databases

--- home/models.py
+++ home/models.py
@@ -5,8 +5,11 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     slug = models.SlugField()
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    # Note:
+    # <auto_now_add> will only save date and time when the record is created and
+    # <auto_now> will save date and time each time the record changes.
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return f"{self.id}- {self.user}, {self.slug}"
