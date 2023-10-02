94b09245a7cef78d562cd583b2e128d75f6a646a
misammehmannavaz <misammehmannavaz@gmail.com>
2023-10-02 18:13:27 +0330

    Customizing the page of Post model in django admin.

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Post
 
-# Register your models here.
-admin.site.register(Post)
+#Customizing the page of Post model in django admin.
+class PostAdmin(admin.ModelAdmin):
+    # These fields will be shown instaed of __str__() method of Post model.
+    list_display = ('user', 'slug', 'updated')
+    search_fields = ('slug', 'body')
+    # the filter that is on the right side of the page.
+    list_filter = ('updated',)
+    # with prepopulated_fields django will fill slug field base on body field, starts from
+    # first, maximum length is 50 charachters.
+    prepopulated_fields = {'slug':('body',)}
+    # for user field we can select users base on its id.Is good for when the number of 
+    # users are a lot.
+    raw_id_fields = ('user',)
+
+admin.site.register(Post, PostAdmin)
