e767b0c43f2835dd622b5a505a300cd89556925a
misammehmannavaz <misammehmannavaz@gmail.com>
2023-10-03 21:41:51 +0330

    Implement delete a post.

--- home/views.py
+++ home/views.py
@@ -1,15 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+
 
 class HomeView(View):
     def get(self, request):
         posts = Post.objects.all()
-        return render(request, 'home/index.html', {'posts': posts})
-    
+        return render(request, "home/index.html", {"posts": posts})
+
 
 class PostDetailView(View):
     def get(self, request, post_id, post_slug):
         post = Post.objects.get(pk=post_id, slug=post_slug)
-        return render(request, 'home/detail.html', {'post': post})
+        return render(request, "home/detail.html", {"post": post})
 
+
+class PostDeleteView(LoginRequiredMixin, View):
+    def get(self, request, post_id):
+        post = Post.objects.get(id=post_id)
+        if post.user.id == request.user.id:
+            post.delete()
+            messages.success(request, "post deleted successfully", "success")
+        else:
+            messages.error(request, "you can not delete this post", "danger")
+        return redirect("home:home")
