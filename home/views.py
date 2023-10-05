924ba1740dcc8637f5de3441b202cadde6bd5644
misammehmannavaz <misammehmannavaz@gmail.com>
2023-10-05 17:25:26 +0330

    Complete updating a post

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from .forms import PostUpdateForm
+from django.utils.text import slugify
 
 
 class HomeView(View):
@@ -35,17 +36,41 @@ class PostDeleteView(LoginRequiredMixin, View):
 class PostUpdateView(LoginRequiredMixin, View):
     form_class = PostUpdateForm
     
+    # setup() method will be ruuning before all other methods like dispatch ,...
+    # We will defind **post_instance** here for making performance better. Here we 
+    # connect to database only one time instead of connecting it in each method.
+    def setup(self, request, *args: Any, **kwargs: Any):
+        self.post_instance = Post.objects.get(pk=kwargs['post_id'])
+        return super().setup(request, *args, **kwargs)
+    
+    # In this method we will not allowing the users that not owned the posts from 
+    # changing them.
     def dispatch(self, request, *args: Any, **kwargs: Any):
-        post = Post.objects.get(pk=kwargs['post_id'])
+        post = self.post_instance
         if post.user.id != request.user.id:
             messages.error(request, 'you are not allowed to update this post', 'error')
             return redirect('home:home')
         return super().dispatch(request, *args, **kwargs)
     
-    def get(self, request, post_id):
-        post = Post.objects.get(pk=post_id)
+    # **post_id** will be in kwargs. We do not need post_id but we have to get it in 
+    # our methods(it comes with calling the url of this api).
+    def get(self, request, *args, **kwargs):
+        post = self.post_instance
         form = self.form_class(instance=post)
         return render(request, 'home/update.html', {'form': form})
     
-    def post(self, request, post_id):
-        pass
+    # **post_id** will be in kwargs. We do not need post_id but we have to get it in 
+    # our methods(it comes with calling the url of this api).
+    def post(self, request, *args, **kwargs):
+        post = self.post_instance
+        form = self.form_class(request.POST, instance=post)
+        if form.is_valid():
+            # We have to save slug manually, because it will not be generated 
+            # automatically.
+            new_post = form.save(commit=False)
+            new_post.slug = slugify(form.cleaned_data['body'][:30])
+            new_post.save()
+            messages.success(request, 'You successfully updated this post', 'success')
+            return redirect('home:post_detail', post.id, post.slug)
+        
+        
