b94534e60ad45bdee4ddc3b501703f3853d998da
misammehmannavaz <misammehmannavaz@gmail.com>
2023-10-10 23:38:23 +0330

    Implement reply form

--- home/views.py
+++ home/views.py
@@ -2,10 +2,10 @@ from typing import Any
 from django import http
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
-from .models import Post
+from .models import Post, Comment
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
-from .forms import PostCreateUpdateForm, CommentCreateForm
+from .forms import PostCreateUpdateForm, CommentCreateForm, CommentReplyForm
 from django.utils.text import slugify
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -21,9 +21,10 @@ class HomeView(View):
 
 class PostDetailView(View):
     form_class = CommentCreateForm
+    form_class_reply = CommentReplyForm
     
     def setup(self, request, *args: Any, **kwargs: Any):
-        # **Note: we can't use self.post because django will this that this is Post model.
+        # **Note: we can't use self.post because django thinks that this is the Post model.
         self.post_instance = get_object_or_404(Post, pk=kwargs['post_id'], slug=kwargs['post_slug'])
         return super().setup(request, *args, **kwargs)
     
@@ -31,7 +32,8 @@ class PostDetailView(View):
     def get(self, request, *args: Any, **kwargs: Any):
         comments = self.post_instance.pcomments.filter(is_reply=False)
         return render(request, "home/detail.html", {"post": self.post_instance, "comments": comments,
-                                                    "form": self.form_class})
+                                                    "form": self.form_class,
+                                                    'reply_form':self.form_class_reply})
     
     @method_decorator(login_required)
     def post(self, request, *args, **kwargs):
@@ -116,4 +118,19 @@ class PostCreateView(LoginRequiredMixin, View):
             new_post.save()
             messages.success(request, 'you successfully created a post','success')
             return redirect('home:post_detail', new_post.id, new_post.slug)
-  
+
+class PostAddReplyView(View):
+    form_class = CommentReplyForm
+    def post(self, request, post_id, comment_id):
+        post = get_object_or_404(Post, id=post_id)
+        comment = get_object_or_404(Comment, id=comment_id)
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            reply = form.save(commit=False)
+            reply.user = request.user
+            reply.post = post
+            reply.reply = comment
+            reply.is_reply = True
+            reply.save()
+            messages.success(request, 'Your reply submitted', 'success')
+        return redirect('home:post_detail', post.id, post.slug)
